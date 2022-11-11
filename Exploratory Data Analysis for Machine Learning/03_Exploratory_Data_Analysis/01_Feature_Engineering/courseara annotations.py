d285c9156d230ebe4635ac8f86ebd58105b6362d
peterfur <pedro.furelosfabeiro@gmail.com>
2022-11-11 12:42:40 +0100

    update

---
+++ Exploratory Data Analysis for Machine Learning/03_Exploratory_Data_Analysis/01_Feature_Engineering/courseara annotations.py
@@ -0,0 +1,10 @@
+#itinerario de garantia xuvenil
+
+#encode categorical variables
+from sklearn.preprocessing import LabelEncoder, LabelBinarizer, OneHotEncoder
+from pandas import get_dummies
+
+#encode ordeinal variables
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.preprocessing import OrdeinalEncoder
